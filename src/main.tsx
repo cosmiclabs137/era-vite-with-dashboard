8058635f660ec2f6f869003600adf6e80c37ae78
jay3ss <mentoce@gmail.com>
2024-10-24 16:32:08 -0700

    First commit

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { RouterProvider } from "react-router-dom";
+// import App from '@/App';
+import theme from "@/theme";
+
+import { router } from "@/router";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </React.StrictMode>
+);
---
+++ src/router.tsx
@@ -0,0 +1,5 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import App from "./App";
+
+export const router = createBrowserRouter([{ path: "/", element: <App /> }]);
