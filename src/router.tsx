e23935f9f81a0f2b6a74689bc60c430db6d513bd
jay3ss <mentoce@gmail.com>
2024-10-24 20:24:46 -0700

    Add dashboard

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import Dashboard from "./dashboard/Dashboard";
 
-export const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+export const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/dashboard", element: <Dashboard /> },
+]);
