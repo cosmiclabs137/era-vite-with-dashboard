8f0a050a783802d67f8d00218b060d1701c8af0f
jay3ss <mentoce@gmail.com>
2024-10-25 15:57:47 -0700

    Add more routes

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import App from "./App";
-import Dashboard from "./dashboard/Dashboard";
+import App from "@/App";
+import Dashboard from "@/dashboard/Dashboard";
+import DealsWrapper from "@/components/Deals/DealsWrapper";
+import ErrorPage from "@/error";
+import MainGrid from "@/dashboard/components/MainGrid";
+
+const dashboardRoutes = [
+  { path: "", element: <MainGrid /> },
+  { path: "deals", element: <DealsWrapper /> },
+];
 
 export const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/dashboard", element: <Dashboard /> },
+  { path: "/", element: <App />, errorElement: <ErrorPage /> },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+    children: dashboardRoutes,
+    errorElement: <ErrorPage />,
+  },
 ]);
