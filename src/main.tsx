42b85eed015dc1fee81bc1b5aa0042d747b4645d
jay3ss <mentoce@gmail.com>
2024-10-24 20:24:09 -0700

    Add store provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,21 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
+import { Provider as ReduxProvider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-// import App from '@/App';
-import theme from "@/theme";
 
-import { router } from "@/router";
+import { store } from "./store";
+import theme from "./theme";
+
+import { router } from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <RouterProvider router={router} />
+      <ReduxProvider store={store}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </ReduxProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
