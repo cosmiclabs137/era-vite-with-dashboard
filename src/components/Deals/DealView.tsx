8b118af8dd4e92e0e2cedc5654986f3421c6b076
jay3ss <mentoce@gmail.com>
2024-10-26 18:56:25 -0700

    Update deals view and wrapper

--- src/components/Deals/DealView.tsx
+++ src/components/Deals/DealView.tsx
@@ -1,25 +1,32 @@
 import React from "react";
-import { List, ListItem, ListItemText } from "@mui/material";
+import { Box, List, ListItem, ListItemText, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 import { useLoaderData } from "react-router";
 
-import { Deal } from "@/lib/definitions";
+import ProposalCard from "@/components/Proposals/ProposalCard";
+import { Deal, Proposal } from "@/lib/definitions";
 
 const DealView = () => {
-  const deal = useLoaderData();
-  // const deal = {id: "79", proposals: []}
-  console.log("deal:", deal);
+  const deal = useLoaderData() as Deal;
+  const hasProposals = deal.proposals.length > 0;
+
   return (
-    <div>
-      <List>
-        <ListItem style={{ margin: 0 }} alignItems="flex-start">
-          {/* @ts-ignore */}
-          <ListItemText>ID: {deal?.id}</ListItemText>
-        </ListItem>
-        <ListItem alignItems="flex-start">
-          {/* @ts-ignore */}
-          <ListItemText>Proposals: {deal?.proposals?.length}</ListItemText>
-        </ListItem>
-      </List>
+    <div style={{ width: "100%" }}>
+      <Typography variant="h2" sx={{ mb: 2 }}>
+        {deal.name}
+      </Typography>
+      <Typography variant="h4">Proposals:</Typography>
+      <Box sx={{ flexGrow: 0, mt: 5 }}>
+        <Grid container spacing={{ xs: 2, md: 3 }}>
+          {hasProposals ? (
+            deal.proposals.map((proposal: Proposal) => (
+              <ProposalCard proposal={proposal} key={proposal?.id} />
+            ))
+          ) : (
+            <Typography>No proposals yet. Create a one!</Typography>
+          )}
+        </Grid>
+      </Box>
     </div>
   );
 };
--- src/components/Deals/DealsWrapper.tsx
+++ src/components/Deals/DealsWrapper.tsx
@@ -1,7 +1,61 @@
 import React from "react";
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  Divider,
+  Paper,
+  Typography,
+} from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import { useLoaderData } from "react-router";
+
+import { Deal } from "@/lib/definitions";
+import ButtonLink from "@/components/common/ButtonLink";
+
+const DealCard = ({ deal }: { deal: Deal }) => {
+  const href = `/dashboard/deals/${deal.id}`;
+  return (
+    <Paper elevation={3} sx={{ w: "100%" }}>
+      <Card sx={{ w: "100%", p: 2 }} variant="outlined">
+        <CardContent>
+          <Typography
+            gutterBottom
+            sx={{ color: "text.secondary", fontSize: 14 }}
+          >
+            {/* NOTE: maybe make the ID viewable on hover instead? something else? */}
+            {deal.name} (ID: {deal.id})
+          </Typography>
+          <Divider variant="fullWidth" sx={{ m: 1 }} />
+          <Typography>Proposals: {deal.proposals.length}</Typography>
+        </CardContent>
+        <CardActions>
+          <ButtonLink href={href} sx={{ mt: 2 }}>
+            View Deal
+          </ButtonLink>
+        </CardActions>
+      </Card>
+    </Paper>
+  );
+};
 
 const DealsWrapper = () => {
-  return <div>DealsWrapper</div>;
+  const deals: Deal[] = useLoaderData() as Deal[];
+  return (
+    <div style={{ width: "100%" }}>
+      <Typography variant="h2">Deals</Typography>
+      <Box sx={{ flexGrow: 0, mt: 5 }}>
+        <Grid container spacing={{ xs: 2, md: 3 }}>
+          {deals.map((deal: Deal) => (
+            <Grid key={deal.id}>
+              <DealCard deal={deal} key={deal.id} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </div>
+  );
 };
 
 export default DealsWrapper;
