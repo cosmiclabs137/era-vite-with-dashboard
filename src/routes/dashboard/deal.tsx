488a9c82cbe4be4096f5663e1fadaca2a9289b36
jay3ss <mentoce@gmail.com>
2024-10-26 19:00:22 -0700

    Add route to view a deal, add data loader functions

--- src/routes/dashboard/deal.tsx
+++ src/routes/dashboard/deal.tsx
@@ -1,10 +1,18 @@
 import { RouteObject } from "react-router";
 
 import DealsWrapper from "@/components/Deals/DealsWrapper";
+import DealView from "@/components/Deals/DealView";
+import { getDealById, getDeals } from "@/lib/deals";
 
 export const routes: RouteObject[] = [
   {
     path: "deals",
     element: <DealsWrapper />,
+    loader: async () => await getDeals(),
+  },
+  {
+    path: "deals/:dealId",
+    element: <DealView />, // @ts-ignore
+    loader: async ({ params }) => await getDealById(params.dealId),
   },
 ];
