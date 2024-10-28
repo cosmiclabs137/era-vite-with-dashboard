113bc5f45a934a4659bf6a117038bccd30ff0c7f
jay3ss <mentoce@gmail.com>
2024-10-28 12:03:28 -0700

    Move into own app

--- src/components/Deals/DealHeader.tsx
+++
@@ -1,8 +0,0 @@
-import AppHeader, { AppHeaderProps } from "@/components/App/AppHeader";
-
-const DealHeader = (props: AppHeaderProps) => {
-  const { title, link } = props;
-  return <AppHeader title={title} link={link} />;
-};
-
-export default DealHeader;
