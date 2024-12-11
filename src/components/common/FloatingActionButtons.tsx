d7fd54cee5b909d9fb45b7a8b1c3d6f43f007721
jay3ss <mentoce@gmail.com>
2024-12-10 16:12:38 -0800

    Add add and edit type Fabs

---
+++ src/components/common/FloatingActionButtons.tsx
@@ -0,0 +1,64 @@
+import { ReactNode } from "react";
+
+import AddIcon from "@mui/icons-material/Add";
+import EditIcon from "@mui/icons-material/Edit";
+import Box from "@mui/material/Box";
+import Fab from "@mui/material/Fab";
+import { useTheme } from "@mui/material";
+
+// Define the valid color types for the Fab component
+type FabColor = "dark" | "light";
+
+interface BaseFabProps {
+  children: ReactNode;
+  label: string;
+}
+
+function BaseFab({ children, label }: BaseFabProps) {
+  const theme = useTheme();
+
+  const fabColor =
+    theme.palette.mode === "dark"
+      ? theme.palette.primary.main
+      : theme.palette.primary.dark;
+  return (
+    <Box
+      sx={{
+        "& > :not(style)": { m: 1 },
+        position: "fixed",
+        bottom: 16,
+        right: 16,
+      }}
+    >
+      <Fab
+        aria-label={label}
+        sx={{
+          backgroundColor: theme.palette.grey[800],
+          color: theme.palette.primary.contrastText,
+          "&:hover": {
+            backgroundColor: theme.palette.grey[100],
+            color: theme.palette.grey[800],
+          },
+        }}
+      >
+        {children}
+      </Fab>
+    </Box>
+  );
+}
+
+export function AddFab() {
+  return (
+    <BaseFab label="add">
+      <AddIcon />
+    </BaseFab>
+  );
+}
+
+export function EditFab() {
+  return (
+    <BaseFab label="edit">
+      <EditIcon />
+    </BaseFab>
+  );
+}
