24cb608e92d2b0aea22353acced139fe058a519c
jay3ss <mentoce@gmail.com>
2024-12-11 14:53:36 -0800

    Implement function to call endpoint to create new deal

--- src/deals/lib/deals.ts
+++ src/deals/lib/deals.ts
@@ -1,8 +1,12 @@
-import { getDealById as apiGetDealById, getDeals as apiGetDeals } from "@/api";
+import { json } from "react-router";
+
+import {
+  getDealById as apiGetDealById,
+  getDeals as apiGetDeals,
+  createDeal as apiCreateDeal,
+} from "@/api";
 
 import { Deal } from "@/deals/lib/definitions";
-
-import { dealFactory } from "@/deals/lib/utils";
 
 export const getDealById = async (dealId: string): Promise<Deal> => {
   const deal = await apiGetDealById(dealId);
@@ -12,4 +16,17 @@ export const getDealById = async (dealId: string): Promise<Deal> => {
 
 export const getDeals = async (): Promise<Deal[]> => await apiGetDeals();
 
-export const createDeal = async (): Promise<Deal> => dealFactory();
+export const createDeal = async ({ request }: { request: Request }) => {
+  const formData = await request.formData();
+  const name = formData.get("name");
+
+  if (typeof name !== "string" || !name.trim()) {
+    return json({ error: "Deal name is required" }, { status: 400 });
+  }
+
+  const newDeal: Deal = await apiCreateDeal(name);
+
+  console.log();
+
+  return newDeal;
+};
