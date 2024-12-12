fdcb99107ea665c9e7d429f7e6e72ad1c4c03de0
jay3ss <mentoce@gmail.com>
2024-12-11 18:19:07 -0800

    Implement dialog component to pop up for confirmation of deleting deal

--- src/deals/components/DealCard.tsx
+++ src/deals/components/DealCard.tsx
@@ -1,14 +1,84 @@
+import React from "react";
+
 import {
+  Box,
+  Button,
   Card,
   CardActions,
   CardContent,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
   Divider,
   Paper,
   Typography,
 } from "@mui/material";
 
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
+import { useNavigate } from "react-router";
+
 import { Deal } from "@/deals/lib/definitions";
 import ButtonLink from "@/components/common/ButtonLink";
+
+const ActionButtons: React.FC<{ href: string }> = ({ href }) => {
+  const navigate = useNavigate();
+  const [alertOpen, setAlertOpen] = React.useState<boolean>(false);
+
+  const handleClick = () => navigate(`${href}/edit`);
+
+  const handleDeleteClick = () => {
+    setAlertOpen(true);
+  };
+
+  const handleConfirmation = () => {
+    navigate(`${href}/delete`);
+  };
+
+  return (
+    <React.Fragment>
+      <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr",
+          gap: 1,
+        }}
+      >
+        <IconButton onClick={handleClick}>
+          <EditIcon />
+        </IconButton>
+        <IconButton onClick={handleDeleteClick}>
+          <DeleteIcon />
+        </IconButton>
+      </Box>
+      <Dialog
+        open={alertOpen}
+        onClose={handleConfirmation}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">Delete deal?</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            This is irreversible. Once deleted, a deal and its proposals will
+            not recoverable.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setAlertOpen(false)} color="primary">
+            Disagree
+          </Button>
+          <Button onClick={handleConfirmation} color="warning" autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+};
 
 const DealCard = ({ deal }: { deal: Deal }) => {
   const href = `/dashboard/deals/${deal.id}`;
@@ -27,9 +97,18 @@ const DealCard = ({ deal }: { deal: Deal }) => {
           <Typography>Proposals: {deal.proposals.length}</Typography>
         </CardContent>
         <CardActions>
-          <ButtonLink href={href} sx={{ mt: 2 }}>
-            View Deal
-          </ButtonLink>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              width: "100%",
+              mt: 1,
+            }}
+          >
+            <ButtonLink href={href}>View Deal</ButtonLink>
+            <ActionButtons href={href} />
+          </Box>
         </CardActions>
       </Card>
     </Paper>
