f1278305199ff361f6f606a5897dceec15539b0c
jay3ss <mentoce@gmail.com>
2024-12-21 18:11:10 -0800

    Update proposal card to be a table

--- src/proposals/components/ProposalCard.tsx
+++ src/proposals/components/ProposalCard.tsx
@@ -1,15 +1,38 @@
 import React from "react";
 import {
-  Card,
-  CardContent,
-  List,
-  ListItem,
-  ListItemText,
   Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
   Typography,
 } from "@mui/material";
 
 import { Proposal } from "@/deals/lib/definitions";
+
+const key_to_text_map: Record<string, string> = {
+  id: "ID",
+  name: "Name",
+  sqft: "SQFT",
+  term: "Term",
+  baseRent: "Base Rent",
+  annualEscalations: "Annual escalations",
+  opExPerMonthRsf: "OpEx / Mo (RSF)",
+  globalInflation: "Inflation",
+  otherOneTimeLandlordCost: "Other one-time landloard cost",
+  otherOneTimeTenantCost: "Other one-time tenant cost",
+  otherMonthlyLandlordCost: "Other monthly landlord cost",
+  otherMonthlyTenantCost: "Other monthly tenant cost",
+  monthsFreeRent: "Mos. free rent",
+  commissionFirst: "Commision (mos. 1 - 60)",
+  commissionSecond: "Commision (mos. 61+)",
+  tenantDiscountRate: "Tenant discount rate",
+  tiCostPerRsf: "TI cost (per RSF)",
+  tiAllowancePerRsf: "TI allowance (per RSF)",
+  landlordDiscountRate: "Landlord discount rate",
+};
 
 interface ProposalCardProps {
   proposal: Proposal;
@@ -17,26 +40,36 @@ interface ProposalCardProps {
 
 const ProposalCard: React.FC<ProposalCardProps> = ({ proposal }) => {
   return (
-    <Paper elevation={3} sx={{ w: "100%" }}>
-      <Card sx={{ w: "100%", p: 2 }} variant="outlined">
-        <CardContent>
-          <List>
-            <ListItem
-              style={{ margin: 0 }}
-              alignItems="flex-start"
-              key="proposal-id"
-            >
-              <ListItemText>
-                <Typography>ID: {proposal.id}</Typography>
-              </ListItemText>
-            </ListItem>
-            <ListItem alignItems="flex-start" key="proposal-name">
-              <ListItemText>Name: {proposal.name}</ListItemText>
-            </ListItem>
-          </List>
-        </CardContent>
-      </Card>
-    </Paper>
+    <TableContainer
+      component={Paper}
+      sx={{ maxWidth: "30%", p: 2 }}
+      elevation={1}
+    >
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell colSpan={2}>
+              <Typography variant="h6" align="center">
+                {proposal.name}
+              </Typography>
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {Object.entries(proposal).map(
+            ([key, value]: [string, string | number]) =>
+              key !== "id" && key !== "name" ? (
+                <TableRow key={key} sx={{ p: 0, m: 0 }}>
+                  <TableCell sx={{ p: 0.2, m: 0.3 }}>
+                    {key_to_text_map[key]}
+                  </TableCell>
+                  <TableCell sx={{ p: 0.2, m: 0.3 }}>{value}</TableCell>
+                </TableRow>
+              ) : null
+          )}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
